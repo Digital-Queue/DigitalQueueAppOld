e240f40fcec0958cdd9ab8158f03aa6e583e0d51
redouane <redouane.sobaihi@live.com>
2022-04-06 20:19:58 +0700

    fix(): make layout scrollable and fixes

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -31,85 +31,87 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 16, right: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset("assets/login.svg"),
-            const SizedBox(
-              height: 32,
-            ),
-            TextField(
-              controller: _emailTextController,
-              autocorrect: false,
-              enableSuggestions: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                  labelText: "E-mail",
-                  prefixIcon: Icon(Icons.email),
-                  fillColor: Colors.white,
-                  filled: true),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextField(
-              controller: _passwordTextController,
-              autocorrect: false,
-              enableSuggestions: false,
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: const InputDecoration(
-                  labelText: "Password",
-                  prefixIcon: Icon(Icons.password),
-                  fillColor: Colors.white,
-                  filled: true),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              width: 192,
-              height: 48,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Text('Login', style: TextStyle(fontSize: 18)),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Icon(
-                      Icons.login,
-                      size: 32.0,
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset("assets/login.svg"),
+              const SizedBox(
+                height: 32,
+              ),
+              TextField(
+                controller: _emailTextController,
+                autocorrect: false,
+                enableSuggestions: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    labelText: "E-mail",
+                    prefixIcon: Icon(Icons.email),
+                    fillColor: Colors.white,
+                    filled: true),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextField(
+                controller: _passwordTextController,
+                autocorrect: false,
+                enableSuggestions: false,
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: "Password",
+                    prefixIcon: Icon(Icons.password),
+                    fillColor: Colors.white,
+                    filled: true),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: 192,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Text('Login', style: TextStyle(fontSize: 18)),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        Icons.login,
+                        size: 32.0,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/recover-account");
-                },
-                child: const Text(
-                  "Forgot Password?",
-                  style: TextStyle(fontSize: 16),
-                )),
-            TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/register");
-                },
-                child: const Text(
-                  "Don't have account? create one.",
-                  style: TextStyle(fontSize: 16),
-                ))
-          ],
+              const SizedBox(
+                height: 24,
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/recover-account");
+                  },
+                  child: const Text(
+                    "Forgot Password?",
+                    style: TextStyle(fontSize: 16),
+                  )),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/register");
+                  },
+                  child: const Text(
+                    "Don't have account? create one.",
+                    style: TextStyle(fontSize: 16),
+                  ))
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/recover_account.dart
+++ lib/pages/recover_account.dart
@@ -26,64 +26,67 @@ class _RecoverAccountPageState extends State<RecoverAccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: SizedBox(
-              height: 250,
-              width: 250,
-              child: SvgPicture.asset("assets/mail.svg"),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: SizedBox(
+                height: 250,
+                width: 250,
+                child: SvgPicture.asset("assets/mail.svg"),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16, right: 16),
-            child: Column(
-              children: [
-                TextField(
-                  controller: _emailFieldController,
-                  autocorrect: false,
-                  enableSuggestions: false,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                      labelText: "Your email",
-                      prefixIcon: Icon(Icons.email),
-                      fillColor: Colors.white,
-                      filled: true),
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                SizedBox(
-                  width: 210,
-                  height: 48,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/reset-password");
-                    },
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: const [
-                        Text('Send Reset Code', style: TextStyle(fontSize: 18)),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Icon(
-                          Icons.send,
-                          size: 32.0,
-                        ),
-                      ],
-                    ),
+            const SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _emailFieldController,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                        labelText: "Your email",
+                        prefixIcon: Icon(Icons.email),
+                        fillColor: Colors.white,
+                        filled: true),
                   ),
-                )
-              ],
-            ),
-          )
-        ],
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  SizedBox(
+                    width: 210,
+                    height: 48,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/reset-password");
+                      },
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Text('Send Reset Code',
+                              style: TextStyle(fontSize: 18)),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Icon(
+                            Icons.send,
+                            size: 32.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
