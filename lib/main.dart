c17c64168fec214ae6a49c0d41e4fd0fd6ab9f1e
redouane <redouane.sobaihi@live.com>
2022-05-05 22:46:57 +0700

    feat(): update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,109 @@
 import 'package:digital_queue/bindings/application_binding.dart';
-import 'package:digital_queue/pages/confirm_email.dart';
-import 'package:digital_queue/pages/profile.dart';
-import 'package:digital_queue/pages/login.dart';
-import 'package:digital_queue/pages/recover_account.dart';
-import 'package:digital_queue/pages/register.dart';
-import 'package:digital_queue/pages/reset_password.dart';
+import 'package:digital_queue/controllers/main_controller.dart';
+import 'package:digital_queue/models/user.dart';
+import 'package:digital_queue/models/user_type_adapter.dart';
+import 'package:digital_queue/pages/auth/auth.dart';
+import 'package:digital_queue/pages/auth/set_name.dart';
+import 'package:digital_queue/pages/auth/verify_auth.dart';
+import 'package:digital_queue/pages/auth/profile.dart';
+import 'package:digital_queue/pages/change_email/change_email.dart';
+import 'package:digital_queue/pages/change_email/confirm_code.dart';
+import 'package:digital_queue/pages/queue/queue.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
+  Hive.registerAdapter<User>(UserAdapter());
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final controller = Get.put(MainController());
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations(
-        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+      [
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ],
+    );
 
     return GetMaterialApp(
       title: 'Digital Queue',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          scaffoldBackgroundColor: Colors.indigo[50]),
+        primarySwatch: Colors.indigo,
+        scaffoldBackgroundColor: Colors.indigo[50],
+      ),
       home: FutureBuilder(
-          future: initialize(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                return const LoginPage();
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              if (snapshot.data == null) {
+                // we do not have user data, user needs to
+                // authenticate.
+                return AuthPage();
+              }
 
-              default:
-                return Scaffold(
-                  body: Center(
-                    child: SvgPicture.asset("assets/logo.svg"),
+              // We have a user, go to profile view.
+              return ProfilePage();
+
+            default:
+              return Scaffold(
+                body: Center(
+                  child: SvgPicture.asset(
+                    "assets/logo.svg",
                   ),
-                );
-            }
-          }),
+                ),
+              );
+          }
+        },
+      ),
       getPages: [
-        GetPage(name: "/login", page: () => const LoginPage()),
-        GetPage(name: "/register", page: () => const RegisterPage()),
         GetPage(
-            name: "/recover-account", page: () => const RecoverAccountPage()),
-        GetPage(name: "/reset-password", page: () => const ResetPasswordPage()),
-        GetPage(name: "/profile", page: () => ProfilePage()),
-        GetPage(name: "/confirm-email", page: () => ConfirmEmailPage())
+          name: "/auth",
+          page: () => AuthPage(),
+        ),
+        GetPage(
+          name: "/verifyAuth",
+          page: () => VerifyAuthPage(),
+        ),
+        GetPage(
+          name: "/profile",
+          page: () => ProfilePage(),
+        ),
+        GetPage(
+          name: "/setName",
+          page: () => SetNamePage(),
+        ),
+        GetPage(
+          name: "/changeEmail",
+          page: () => ChangeEmailPage(),
+        ),
+        GetPage(
+          name: "/confirmCode",
+          page: () => ConfirmCodePage(),
+        ),
+        GetPage(
+          name: "/queue",
+          page: () => QueuePage(),
+        )
       ],
       initialBinding: ApplicationBindings(),
     );
-  }
-
-  Future initialize() async {
-    await Future.delayed(const Duration(seconds: 3));
   }
 }
---
+++ lib/pages/change_email/change_email.dart
@@ -0,0 +1,94 @@
+import 'package:digital_queue/controllers/recover_account_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+
+class ChangeEmailPage extends StatelessWidget {
+  const ChangeEmailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.find<RecoverAccountController>();
+
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) => SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: constraints.maxHeight,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: SizedBox(
+                    height: 250,
+                    width: 250,
+                    child: SvgPicture.asset("assets/mail.svg"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                    right: 16,
+                  ),
+                  child: Form(
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: controller.emailTextController,
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                              labelText: "Your new email",
+                              prefixIcon: Icon(Icons.email),
+                              fillColor: Colors.white,
+                              filled: true),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        SizedBox(
+                          width: 210,
+                          height: 48,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Get.toNamed("/confirmCode");
+                            },
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: const [
+                                Text(
+                                  'Send Code',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Icon(
+                                  Icons.send,
+                                  size: 32.0,
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
