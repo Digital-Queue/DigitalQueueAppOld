4fd931fc556d226eb914a313f1dc773d43fc0263
redouane <redouane.sobaihi@live.com>
2022-05-13 22:42:06 +0700

    feat(): initialize session on app start
    - add session init flow
    - fix dependencies

--- lib/bindings/application_binding.dart
+++ lib/bindings/application_binding.dart
@@ -1,12 +1,18 @@
+import 'package:digital_queue/controllers/main_controller.dart';
 import 'package:digital_queue/controllers/profile_controller.dart';
 import 'package:digital_queue/controllers/auth_controller.dart';
 import 'package:digital_queue/controllers/set_name_controller.dart';
 import 'package:digital_queue/controllers/verify_auth_controller.dart';
+import 'package:digital_queue/services/api_client.dart';
+import 'package:digital_queue/services/user_service.dart';
 import 'package:get/get.dart';
 
 class ApplicationBindings extends Bindings {
   @override
   void dependencies() {
+    // Get.put(UserService());
+    // Get.put(ApiClient());
+    // Get.put(MainController());
     Get.put(AuthController());
     Get.put(VerifyAuthController());
     Get.put(SetNameController());
